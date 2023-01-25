da08aa85f0ae199cdf02f16ca0ca8922777cc7a1
Maes Constantin <constantinmaes@gmail.com>
2023-01-25 20:48:15 +0100

    ✨ feat(appointments): add vamidation schema

--- controllers/appointments.controller.js
+++ controllers/appointments.controller.js
@@ -1,3 +1,5 @@
+const Joi = require('joi');
+
 const dbClient = require('../utils/').dbClient;
 const database = dbClient.db(process.env.MONGO_DB_DATABASE);
 const collection = database.collection('appointments');
@@ -24,6 +26,21 @@ exports.findAll = async (req, res) => {
     ]);
 };
 exports.findOne = async (req, res) => {};
-exports.create = async (req, res) => {};
+exports.create = async (req, res) => {
+    // end: date+h + futur + requis + postérieure à start
+    // start: date+h + futur + requis
+    // subject: string + max 150 + requis
+    // description: pas obligatoire + string + max 1500
+    // location: string + requis
+    // participants: array > string + min 2 elements
+    const schema = Joi.object({
+        start: Joi.date().min('now').required(),
+        end: Joi.date().greater(Joi.ref('start')).required(),
+        subject: Joi.string().max(150).required(),
+        description: Joi.string().max(1500),
+        location: Joi.string().required(),
+        participants: Joi.array().items(Joi.string()).min(2),
+    });
+};
 exports.updateOne = async (req, res) => {};
 exports.deleteOne = async (req, res) => {};
