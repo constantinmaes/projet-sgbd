1d019110f16820dde2c8c13fd2a7cecbbbc94dd0
Maes Constantin <constantinmaes@gmail.com>
2023-01-25 20:14:21 +0100

    ✨ feat(appointments): add aggregation

--- controllers/appointments.controller.js
+++ controllers/appointments.controller.js
@@ -2,7 +2,27 @@ const dbClient = require('../utils/').dbClient;
 const database = dbClient.db(process.env.MONGO_DB_DATABASE);
 const collection = database.collection('appointments');
 
-exports.findAll = async (req, res) => {};
+exports.findAll = async (req, res) => {
+    const data = await collection.aggregate([
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'participants',
+                foreignField: '_id',
+                as: 'populatedParticipants',
+            },
+        },
+        {
+            $project: {
+                location: 1,
+                subject: 1,
+                start: 1,
+                end: 1,
+                participants: '$populatedParticipants',
+            },
+        },
+    ]);
+};
 exports.findOne = async (req, res) => {};
 exports.create = async (req, res) => {};
 exports.updateOne = async (req, res) => {};
