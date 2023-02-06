dfa72af4cfdbcd55d6fa214d3c6cee245dbcd661
Maes Constantin <constantinmaes@gmail.com>
2023-02-06 11:23:55 +0100

    🐛 fix(appointments): add missin toArray

--- controllers/appointments.controller.js
+++ controllers/appointments.controller.js
@@ -5,25 +5,27 @@ const database = dbClient.db(process.env.MONGO_DB_DATABASE);
 const collection = database.collection('appointments');
 
 exports.findAll = async (req, res) => {
-    const data = await collection.aggregate([
-        {
-            $lookup: {
-                from: 'users',
-                localField: 'participants',
-                foreignField: '_id',
-                as: 'populatedParticipants',
+    const data = await collection
+        .aggregate([
+            {
+                $lookup: {
+                    from: 'users',
+                    localField: 'participants',
+                    foreignField: '_id',
+                    as: 'populatedParticipants',
+                },
             },
-        },
-        {
-            $project: {
-                location: 1,
-                subject: 1,
-                start: 1,
-                end: 1,
-                participants: '$populatedParticipants',
+            {
+                $project: {
+                    location: 1,
+                    subject: 1,
+                    start: 1,
+                    end: 1,
+                    participants: '$populatedParticipants',
+                },
             },
-        },
-    ]);
+        ])
+        .toArray();
 
     return res.status(200).json(data);
 };
